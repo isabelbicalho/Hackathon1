627adb8efff89f44a7fc4062e679777a7d6e5078
patigsilva <pat.g.silva@gmail.com>
2016-09-18 12:18:58 -0300

    10 receitas!

--- Hackathon1/Banco.cs
+++ Hackathon1/Banco.cs
@@ -10,7 +10,7 @@ namespace Hackathon1
     {
         public Dictionary<string,Usuario> usuarios = new Dictionary<string, Usuario>();
         public List<Restaurante> restaurantes = new List<Restaurante>();
-        public List<Alimento> alimentos = new List<Alimento>();
+        public List<Receita> receitas = new List<Receita>();
         //public List<string> usuarioSaudacao = new List<string>();
 
         public Dictionary<int, String> respostas = new Dictionary<int, string>()
