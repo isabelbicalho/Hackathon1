19d5ff604987fc4afb5b3d0b62ad78e2fcfdfd8d
Yussif Barcelos <barcelosyussif@gmail.com>
2016-09-17 21:56:36 -0300

    usuario

--- Hackathon1/Usuario.cs
+++ Hackathon1/Usuario.cs
@@ -10,7 +10,39 @@ namespace Hackathon1
     {
         public string id;
         public string nome;
+        public int estado = 0;
+        //pedido atual
         public string endereco;
-        public int estado = 0;      
+        public string cidade;
+        public string bairro;
+        public int necessidade = 0;
+        public int restricao = 0;
+        public string ingrediente;
+        public string prato;
+
+        public void iniciarPedido()
+        {
+            endereco = "";
+            cidade = "";
+            bairro = "";
+            necessidade = Necessidade.nenhum.GetHashCode();
+            restricao = Restricao.nenhum.GetHashCode();
+            ingrediente = "";
+            prato = "";
+        }
+    }
+    public enum Necessidade
+    {
+        nenhum,
+        receita,
+        restaurante,
+        delivery
+    }
+    public enum Restricao
+    {
+        nenhum,
+        lactose,
+        gluten,
+        acucar
     }
 }
